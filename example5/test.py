b7f5f8be188d2184d68c459235e1c20b55a4b85c
fengyueran <316032603@qq.com>
2017-07-16 18:31:57 +0800

    sofang spider

--- example5/test.py
+++ example5/test.py
@@ -6,26 +6,14 @@ import chardet
 import json
 import codecs
 import zlib
-from bs4 import BeautifulSoup
 
-
-def GetTimeByArticle(url):
-    request = urllib2.Request(url)
-    response = urllib2.urlopen(request)
-    resHtml = response.read()
-    html = etree.HTML(resHtml)
-    time = html.xpath('//span[@class="tail-info"]')[1].text
-    print time
-    return time
 
 
 def main():
     output = codecs.open('soufang.json', 'w', encoding='utf-8')
 
-    for pn in range(1, 40, 1):
-
-        page = pn + 1
-        url = 'http://newhouse.cq.fang.com/house/s/b' + str(pn+90) + '/?ctm=1.cq.xf_search.page.'+ str(page)
+    for pn in range(1, 39, 1):
+        url = 'http://newhouse.cq.fang.com/house/s/b' + str(pn+900) + '/?ctm=1.cq.xf_search.page.14'
         print url
         request = urllib2.Request(url)
         request.add_header('Accept-encoding', 'gzip')
@@ -46,35 +34,38 @@ def main():
         sites = html.xpath('//li/div[@class="clearfix"]')
         # print sites
         for site in sites:
-            address =  site.xpath('.//*[@class="address"]/a')[0].attrib['title'].strip()
-            price = site.xpath('.//*[@class="nhouse_price"]/span/text()[last()]')
+            name = site.xpath('.//*[@class="nlcd_name"]/a')[0].text.strip()
+            address =  site.xpath('.//*[@class="address"]/a')[0].text.strip()
+            district = ''
+            if len(address) > 0:
+                address = address.split(']', 1 );
+                district = address[0].split('[', 1 )[1];
+                address = address[1].strip()
+
             print address
-            print price
-        #     # print etree.tostring(site.xpath('.//a')[0])
-        #     title = site.xpath('.//a')[0].text
-        #     Article_url = site.xpath('.//a')[0].attrib['href']
-        #     reply_date = GetTimeByArticle('http://tieba.baidu.com' + Article_url)
-        #     # strip 去掉空格
-        #     jieshao = site.xpath('.//*[@class="threadlist_abs threadlist_abs_onlyline "]')[0].text.strip()
-        #     author = site.xpath('.//*[@class="frs-author-name j_user_card "]')[0].text.strip()
-        #     lastName = site.xpath('.//*[@class="frs-author-name j_user_card "]')[1].text.strip()
-        #     print title, jieshao, Article_url, author, lastName
-        #
+
+            # print "".join(address[0])
+            # print len(address[0].split('[', 1 ))
+
+
+
+            price = site.xpath('.//*[@class="nhouse_price"]/span/text()[last()]')
+            priceUnit = site.xpath('.//*[@class="nhouse_price"]/em/text()');
+            priceUnit = "".join(priceUnit)
+            price = "".join(price) + ' ' + priceUnit
+            # print address
+            # print name, address, price
+
             item = {}
-        #
+
             item['price'] = price
+            item['district'] = district
             item['address'] = address
-
-        #     item['lastName'] = lastName
-        #     item['reply_date'] = reply_date
-        #     print item
-        #
             line = json.dumps(item, ensure_ascii=False)
-            print line
-            # print type(line)
-
+            # # print line
+            #
             output.write(line + "\n")
-        # output.close()
+    output.close()
     print 'end'
 
 if __name__ == '__main__':
